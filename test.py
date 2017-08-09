edb313a59e77a787559e9240303092cad262af6d
Christian Decker <decker.christian@gmail.com>
2017-08-09 18:56:04 +0200

    tests: Confirm any eventual addfunds transactions

--- test.py
+++ test.py
@@ -149,10 +149,13 @@ def testOpenchannel(bitcoind, node_factory, impls):
     wait_for(lambda: node2.peers(), interval=1)
 
     node1.addfunds(bitcoind, 2 * 10**7)
+    time.sleep(1)
+    bitcoind.rpc.generate(1)
+    time.sleep(1)
     node1.openchannel(node2.id(), 'localhost', node2.daemon.port, 10**7)
     for _ in range(5):
         time.sleep(1)
         bitcoind.rpc.generate(1)
 
-    wait_for(lambda: node1.check_channel(node2))
-    wait_for(lambda: node2.check_channel(node1))
+    wait_for(lambda: node1.check_channel(node2), interval=1, timeout=10)
+    wait_for(lambda: node2.check_channel(node1), interval=1, timeout=10)
