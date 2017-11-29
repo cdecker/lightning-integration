82a922633c520fad8c9c5f6bc183b3cdd109272f
Christian Decker <decker.christian@gmail.com>
2017-11-29 15:04:39 +0100

    eclair: Connect parameters were switched to match c-lightning

--- eclair.py
+++ eclair.py
@@ -183,7 +183,7 @@ class EclairNode(object):
             return result['paymentPreimage']
 
     def connect(self, host, port, node_id):
-        return self.rpc._call('connect', [host, port, node_id])
+        return self.rpc._call('connect', [node_id, host, port])
 
     def info(self):
         r = self.rpc._call('getinfo', [])
